0d05b5d1a0c394076bcc2fd74d51cf7b7abce0cc
boyraad300 <boyraad300@yahoo.com>
2015-09-01 09:16:04 -0400

    Update gbanlogs.lua

--- lua/autorun/client/gbanlogs.lua
+++ lua/autorun/client/gbanlogs.lua
@@ -3,14 +3,15 @@ net.Receive( "devbanlogssvr", function()
 	local steamids = net.ReadString()
 	GBackgroundCheck2(banlinfo,steamids)
 end)
-local function settings() 
+local function sendsettings(tbl) 
 	net.Start("devbanlogscl")
+	net.WriteTable(tbl)
 	net.SendToServer()
 end
 
 function GBackgroundCheck2(baninfo,steamid)
 local tbltoprint = table.KeysFromValue(baninfo.banlogs,steamid)
-local settingstbl = baninfo.settings
+local settingstbl = {}
 local frame = vgui.Create( "DFrame" )
 frame:SetSize( 900, 400 )
 frame:SetTitle("GBanLogs")
@@ -18,10 +19,32 @@ frame:Center()
 frame:MakePopup()
 	local sheet = vgui.Create( "DColumnSheet", frame )
 	sheet:Dock( FILL )
---	local settings = vgui.Create( "DPanel", sheet )
---	settings:Dock( FILL )
---	settings.Paint = function( self, w, h ) draw.RoundedBox( 4, 0, 0, w, h, Color( 255, 255, 255 ) ) end
---	sheet:AddSheet( "Settings", settings, "icon16/bullet_red.png" )
+	local settings = vgui.Create( "DPanel", sheet )
+	settings:Dock( FILL )
+	settings.Paint = function( self, w, h ) draw.RoundedBox( 4, 0, 0, w, h, Color( 255, 255, 255 ) ) end
+	sheet:AddSheet( "Settings", settings, "icon16/bullet_red.png" )
+		local cansee = vgui.Create("DCheckBox",settings)
+		cansee:SetPos(25,50)
+		cansee:SetValue(0)
+		local function cansee:onChange(val)
+			settingstbl.shouldecho = val
+		end
+		local canseehint = vgui.Create("DLabel",settings)
+		canseehint:SizeToContent()
+		canseehint:SetText("Check this if you want the server to inform admins of previous offenders")
+		canseehint:SetPos(25,70)
+		local canseeg = vgui.Create("DTextEntry",settings)
+		canseeg:SetPos(40,50)
+		canseeg:SetSize(70,85)
+		canseeg:SetText("Lowest group that should see the echo (Any groups above this will also see the echo)")
+		local function canseeg:onChange(str)
+			settingstbl.canseeecho = str
+		end
+		local saveb = vgui.Create("DButton",settings)
+		settings:SetSize(25,25)
+		settings:SetText("Save")
+		settings.DoClicked = function()
+			sendsettings(settingstbl)
 	for k, v in pairs(tbltoprint)do 
 	local panel = vgui.Create( "DPanel", sheet )
 	panel:Dock( FILL )
