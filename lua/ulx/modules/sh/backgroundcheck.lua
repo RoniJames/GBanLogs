5d6bb16b69849a11bdf67bc4600ae5cff681ae2b
boyraad300 <boyraad300@yahoo.com>
2015-09-08 23:17:04 -0400

    experimental

--- lua/ulx/modules/sh/backgroundcheck.lua
+++ lua/ulx/modules/sh/backgroundcheck.lua
@@ -1,14 +1,17 @@
 function ulx.backgroundcheck(calling_ply,steamid)
 	local banlogs  
-	if file.Read("gbanlogs/banlogs.txt","DATA") == "" or nil then
+	if file.Read("gbanlogs/banlogs.txt","DATA") then
 		banlogs = {}
 	else
-		banlogs = util.JSONToTable(file.Read("goku/banlogs.txt","DATA"))
+		banlogs = util.JSONToTable(file.Read("gbanlogs/banlogs.txt","DATA"))
+	end
+	local etc = file.Read("gbanlogs/etclogs.txt","DATA")
+	banlogs.etc = etc.steamid
 	net.Start("devbanlogssvr")
 	net.WriteTable(banlogs)
 	net.WriteString(steamid)
 	net.Send(calling_ply)
-	end
+	
 end
 local backgroundcheck = ulx.command("Utility","ulx idcheck",ulx.backgroundcheck,"!check")
 backgroundcheck:addParam{ type=ULib.cmds.StringArg, hint="steamid" } 
