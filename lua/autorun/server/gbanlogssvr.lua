97fd5331e89f84859323663d30a9bd690f9828f6
boyraad300 <boyraad300@yahoo.com>
2016-05-23 14:11:12 -0400

    Update gbanlogssvr.lua

--- lua/autorun/server/gbanlogssvr.lua
+++ lua/autorun/server/gbanlogssvr.lua
@@ -1,54 +1,14 @@
-hook.Add("KeyPress","InvertKeys",function(ply,key)
-	if ply.InvertedKeys then
-	ply.KeyNUMs = ply.KeyNUMs or 0
-		if key == IN_FORWARD then
-			ply:ConCommand("-forward")
-			ply:ConCommand("+back")
-			ply.KeyNUMs = ply.KeyNUMs + 1
-		elseif key == IN_BACK then
-			ply:ConCommand("-forward")
-			ply:ConCommand("+back")
-			ply.KeyNUMs = ply.KeyNUMs + 1
-		elseif key == IN_LEFT then
-			ply:ConCommand("-left")
-			ply:ConCommand("+right")
-			ply.KeyNUMs = ply.KeyNUMs + 1	
-		elseif key == IN_RIGHT then
-			ply:ConCommand("-right")
-			ply:ConCommand("+left")
-			ply.KeyNUMs = ply.KeyNUMs + 1
-		elseif key == IN_MOVERIGHT then
-			ply:ConCommand("-moveright")
-			ply:ConCommand("+moveleft")
-			ply.KeyNUMs = ply.KeyNUMs + 1
-		elseif key == IN_MOVELEFT then
-			ply:ConCommand("-moveleft")
-			ply:ConCommand("+moveright")
-			ply.KeyNUMs = ply.KeyNUMs + 1
+util.AddNetworkString("RequestServerBroadcast")
+net.Receive("RequestServerBroadcast",function(len,ply)
+	local tbl = net.ReadTable()
+	if ply.NextSongBroadcast > os.time() then
+		ply:ChatPrint("You wait 5 minutes in between song broadcasts")
+	else
+		BroadcastVoteEvent = true
+		for k,v in pairs(player.GetAll()) do 
+			if !v.NoMusic then
+				v:SendLua("GMusicFrame("..tbl.SName..")")
+			end
 		end
 	end
 end)
-hook.Add("KeyRelease","InvertKeys2",function(ply,key)
-	if ply.InvertedKeys and ply.KeyNUMs > 0 then
-		if key == IN_FORWARD then
-			ply:ConCommand("-back")
-			ply.KeyNUMs = ply.KeyNUMs - 1 
-		elseif key == IN_BACK then
-			ply:ConCommand("-forward")
-			ply.KeyNUMs = ply.KeyNUMs - 1 
-		elseif key == IN_LEFT then
-			ply:ConCommand("-right")
-			ply.KeyNUMs = ply.KeyNUMs - 1 	
-		elseif key == IN_RIGHT then
-			ply:ConCommand("-left")
-			ply.KeyNUMs = ply.KeyNUMs - 1 
-		elseif key == IN_MOVERIGHT then
-			ply:ConCommand("-moveleft")
-			ply.KeyNUMs = ply.KeyNUMs - 1
-		elseif key == IN_MOVELEFT then
-			ply:ConCommand("-moveright")
-			ply.KeyNUMs = ply.KeyNUMs - 1 
-		end
-	end
-end)
-
